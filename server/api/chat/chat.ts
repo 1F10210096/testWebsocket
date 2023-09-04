1c8824fac8d0cd3cb9fd662262dffce3da43977a
1F10210096 <s1F102100962@iniad.org>
2023-09-05 05:54:05 +0900

    w!

--- server/api/chat/chat.ts
+++ server/api/chat/chat.ts
@@ -6,7 +6,7 @@ try {
   const server: http.Server = http.createServer();
   const io = new Server(server, {
     cors: {
-      origin: 'http://localhost:3000',
+      origin: 'http://192.168.128.199:3000',
     },
   });
 
